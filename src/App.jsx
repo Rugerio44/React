2551231323eaff2fa3a37d25d635037667b966d2
Rugerio44 <gianni.rugerio@hotmail.com>
2024-04-11 17:28:07 -0600

    componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import MiComponente from './MiComponente.js';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,11 +25,15 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+
+      {/*Cargar el primer componente*/}
+      <MiComponente />
+    </> 
   )
 }
 
---
+++ src/MiComponente.js
@@ -0,0 +1,8 @@
+//Importar modulos de react
+import React from "react";
+
+//función del componente 
+const MiComponente = () => {
+    return <p>Este primer componente</p>;
+};
+export default MiComponente;
