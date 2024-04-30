0f4ed60979f0fa1cdfa13b6fcd974cd8307acf2a
Lezama44 <gianni.rugerio@hotmail.com>
2024-04-30 17:37:11 -0600

    mensaje de error

--- Proyecto_videos/ProyectoVideos/src/Componentes/Buscador.jsx
+++ Proyecto_videos/ProyectoVideos/src/Componentes/Buscador.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 export const Buscador = ({listadoState,setlistadoState}) => {
 
   const[busqueda,setbusqueda] = useState('');
+  const[noEncontrado,setnoEncontrado] = useState(false);
 
   const buscarpeli = (e) => {
     //Crear estado y actualizarlo 
@@ -15,12 +16,16 @@ export const Buscador = ({listadoState,setlistadoState}) => {
 
     if(busqueda.length <= 1 || peliculaencontrada <= 0){
       peliculaencontrada = JSON.parse(localStorage.getItem("pelicula"));
+      setnoEncontrado(true);
+    }else{
+      setnoEncontrado(false);
     }
     
     
     setlistadoState(peliculaencontrada);
     //Actualizar el listado de peliculas
-    //setlistadoState()
+    
+
 
   }
 
@@ -29,6 +34,7 @@ export const Buscador = ({listadoState,setlistadoState}) => {
     
         <div className="search">
                 <h3 className="title">Buscador: {busqueda}</h3>
+                <span className='no-encontrado'></span>
                 <form>
                     <input type="text" 
                            id="search_field"
