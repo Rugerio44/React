57755651920fa67ff8b808f86eec1a302c38e67e
Lezama44 <gianni.rugerio@hotmail.com>
2024-05-03 17:08:52 -0600

    agregamos el router y los elementos con el dom

---
+++ Proyecto7/proyecto7-routers/src/components/Articulos.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Articulos = () => {
+  return (
+    <div>
+        <h1>pagina de Articulos</h1>
+        <p>Primera Página de Articulos</p>
+    </div>
+  )
+}
