fb66151d62dd78725c939d55689152e0caeb70da
Lezama44 <gianni.rugerio@hotmail.com>
2024-05-14 15:19:57 -0600

    terminar las rutas con sub-rutas y hacer un menu interactivo

--- Proyecto7/proyecto7-routers/src/components/Persona.jsx
+++ Proyecto7/proyecto7-routers/src/components/Persona.jsx
@@ -8,27 +8,36 @@ export const Persona = () => {
 
   const enviar = (e) => {
     e.preventDefault();
-    let nombre = e.target.nombre?.value; // Usa el operador opcional de encadenamiento (?.)
+    let nombre = e.target.nombre?.value;
     let apellido = e.target.apellido?.value;
     let url = `/persona/${nombre}/${apellido}`;
 
-    console.log(url);
-    navegar(url);
+    if(nombre.length <= 0 && apellido.length <= 0){
+      navegar("/inicio");
+    }else if(nombre === "contacto"){
+      navegar("/contacto");
+    }else{
+      navegar(url);
+    }
+
   };
 
   return (
     <div>
-        <h1>Pagina de Persona</h1>
         {!nombre && <h2>No hay personas que mostrar aqui</h2>}
         {nombre && <h2>Página de Persona: {nombre} {apellido}</h2>}
+
         <p>Esta es la Página de Persona</p>
 
         <form onSubmit={enviar}>
-          <input type="text" placeholder="Nombre" />
-          <input type="text" placeholder="Apellido" />
-          <input type="submit" value="Enviar" />
+          <input type="text" name="nombre" placeholder="Nombre" />
+          <input type="text" name="apellido" placeholder="Apellido" />
+          <input type="submit" name="enviar" value="Enviar" />
         </form>
 
     </div>
   )
-}
+} 
+
+
+
