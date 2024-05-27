8a8b52907b599b3ae17bc4080ad2f66ac496f1bb
Lezama44 <gianni.rugerio@hotmail.com>
2024-05-27 17:33:28 -0600

    agregar el portafolio con las imagenes y muy poco del estilo de cada una de las imagenes

--- ProyectoPorta/Proyecto1/src/components/Portafolio.jsx
+++ ProyectoPorta/Proyecto1/src/components/Portafolio.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { trabajo } from '../data/Trabajo';
+import {Link} from 'react-router-dom'
 
 export const Portafolio = () => {
   return (
     <div className='page'>
       <h1 className='heading'>Portafolio</h1>
+
+      <section className="works">
+      {
+        trabajo.map(trabajo =>(
+        <article key={trabajo.id} className='works__work-item'>
+          <hr />
+          <div className="work-item__mask">
+            <img src={"/img/"+trabajo.id+".jpg"} className='mask__imagen'/>
+          </div>
+          <span>{trabajo.categorias}</span>
+          <h2> <Link to={"/proyecto/"+trabajo.id}> {trabajo.nombre}</Link></h2>
+          <p>Tecnologías: {trabajo.tecnologias}</p>
+        </article>
+      ))
+      }
+      </section>
+
     </div>
   )
 }
---
+++ ProyectoPorta/Proyecto1/src/data/Trabajo.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+export const trabajo = [
+    {
+        'id' : 'giannifrancescoweb',
+        'nombre' : 'Gianni Francesco Rugerio' ,
+        'url' : 'gianni.rugerio.es' ,
+        'tecnologias' : 'Html,CSS,JS',
+        'categorias' : 'Desarrollo Web',
+        'descripcion' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus recusandae quam cum dignissimos tempora assumenda tenetur magni. Modi veniam corporis itaque ipsa saepe. Tempore aut error totam? Hic, beatae perferendis?'
+    },
+    {
+        'id' : 'facebook',
+        'nombre' : 'Facebook' ,
+        'url' : 'facebook.com' ,
+        'tecnologias' : 'Html,CSS,JS,PHP',
+        'categorias' : 'Desarrollo Web',
+        'descripcion' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus recusandae quam cum dignissimos tempora assumenda tenetur magni. Modi veniam corporis itaque ipsa saepe. Tempore aut error totam? Hic, beatae perferendis?'
+    },
+    {
+        'id' : 'google',
+        'nombre' : 'Google' ,
+        'url' : 'google.com' ,
+        'tecnologias' : 'React, Node, Mongo',
+        'categorias' : 'Desarrollo Web',
+        'descripcion' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus recusandae quam cum dignissimos tempora assumenda tenetur magni. Modi veniam corporis itaque ipsa saepe. Tempore aut error totam? Hic, beatae perferendis?'
+    },
+    {
+        'id' : 'twitter',
+        'nombre' : 'Twitter' ,
+        'url' : 'twitter.com' ,
+        'tecnologias' : 'Html,CSS',
+        'categorias' : 'Desarrollo Web',
+        'descripcion' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus recusandae quam cum dignissimos tempora assumenda tenetur magni. Modi veniam corporis itaque ipsa saepe. Tempore aut error totam? Hic, beatae perferendis?'
+    },
+    {
+        'id' : 'instagram',
+        'nombre' : 'Instagram' ,
+        'url' : 'instagram.com' ,
+        'tecnologias' : 'React, Node, Mongo',
+        'categorias' : 'Desarrollo Web',
+        'descripcion' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus recusandae quam cum dignissimos tempora assumenda tenetur magni. Modi veniam corporis itaque ipsa saepe. Tempore aut error totam? Hic, beatae perferendis?'
+    },
+    {
+        'id' : 'youtube',
+        'nombre' : 'Youtube' ,
+        'url' : 'youtube.com' ,
+        'tecnologias' : 'JS, React, PHP',
+        'categorias' : 'Desarrollo Web',
+        'descripcion' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus recusandae quam cum dignissimos tempora assumenda tenetur magni. Modi veniam corporis itaque ipsa saepe. Tempore aut error totam? Hic, beatae perferendis?'
+    }
+];
