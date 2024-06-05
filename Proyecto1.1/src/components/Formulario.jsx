d6bc7d038dcd375bfe4d6d29fa6b2ddb18ccc1cd
Lezama44 <gianni.rugerio@hotmail.com>
2024-06-05 17:27:41 -0600

    Agregar el nuevo formulario con el useref y empezar a usar el usestate y el use effect

--- Proyecto1.1/src/components/Formulario.jsx
+++ Proyecto1.1/src/components/Formulario.jsx
@@ -7,6 +7,7 @@ export const Formulario = () => {
     const apellidoValue = useRef();
     const emailValue = useRef();
     const miCaja = useRef();
+    
 
     const mostrar = e => {
         e.preventDefault();
@@ -15,20 +16,25 @@ export const Formulario = () => {
         console.log(nombreValue.current.value);
         console.log(apellidoValue.current.value);
         console.log(emailValue.current.value);
-
+        
         //mi caja
-
-        console.log(miCaja);
+        
+        let {current:caja} = miCaja
+        
+        
+        caja.classList.add("fondoVerde");
+        caja.innerHTML = "<h2>Formulario Enviado<h2/>";
     }
 
   return (
     <div>
-        <h1>Formulario</h1>
+        <h1>Formulario con UseRef</h1>
 
         <form onSubmit={mostrar}>
 
             <div ref={miCaja} className='micaja'>
                 <h2>Pruebas con use ref</h2>
+                
             </div>
 
             <input type="text" placeholder='Nombre' ref={nombreValue} /><br />
@@ -42,3 +48,4 @@ export const Formulario = () => {
     </div>
   )
 }
+ 
