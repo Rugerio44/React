640cfc1718ed572661648784c896145d4cdbef2c
Rugerio44 <gianni.rugerio@hotmail.com>
2024-06-26 17:34:15 -0600

    ya a empezar a usar una bd prueba para jalar datos

--- Proyecto1.2/src/components/Gestion.jsx
+++ Proyecto1.2/src/components/Gestion.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { Empleados } from './Empleados'
 
 export const Gestion = () => {
+
+  const [nombre,setnombre] = useState("");
+  
+  const asignargestor = e => {
+    setnombre(e.target.value);
+  }
+
+  console.log("Vista Gestion Actualizada!!");
+
   return (
     <div>
-        <h1 className='name__boss'>Nombre del Gestor: Gianni</h1>
-        
+        <h1 className='name'>Nombre del Gestor: {nombre}</h1>
+        <div className="name__caja">
+        <input className='name__input' type="text" onChange={asignargestor} placeholder='Introducir tu nombre' />
+        </div>
         <h2 className='name__client'>Listado de empleados:</h2>
+        <p className="name__texto">Los Usuarios se gestionan por {nombre} de jsonplaceholder...</p>
+        <Empleados/>
     </div>
   )
 }
