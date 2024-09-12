391f8c92ce83c73b98bb38ed2363bbe89a521b08
Lezama44 <giannieljefe@hotmail.com>
2024-09-11 21:30:47 -0600

    un form de videojuegos

---
+++ Proyecto1.4/Proyecto1.4/src/Components/Misvideojuegos.jsx
@@ -0,0 +1,73 @@
+import React,{useEffect,useReducer} from 'react';
+import { JuegoReducer } from '../reducers/JuegoReducer';
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('juegos')) || [];
+}
+
+export const Misvideojuegos = () => {
+
+  const [juegos, dispatch] = useReducer(JuegoReducer, [], init);
+
+  useEffect(() => {
+    localStorage.setItem('juegos', JSON.stringify(juegos));
+  }, [juegos]);
+  
+    
+  const conseguirDatosForm = e => {
+    e.preventDefault();
+    
+    let juego = {
+        id: new Date().getTime(),
+        titulo: e.target.titulo.value,
+        descripcion: e.target.descripcion.value
+    };
+
+    console.log(juego);
+    e.target.reset();
+
+    const action = {
+        type: 'AGREGAR_JUEGO',
+        payload: juego
+    };
+
+    dispatch(action);
+
+    console.log(juegos);
+    
+
+  }
+  
+  return (
+    <div>
+        <h1>Estos son mis Videojuegos</h1>
+    
+        <p>Número de videojuegos : {juegos.length}</p>
+
+        <ul>
+            <li>
+                {juegos.map(juego => (
+                    <li key={juego.id}>
+                        {juego.titulo}
+                        &nbsp;<button>X</button>
+                    </li>
+                ))}
+            </li>
+           
+        </ul>
+       
+        <h3>Agregar Juego</h3>
+
+        <form className='formulario' onSubmit={conseguirDatosForm}>
+
+            <input type="text" name='titulo' placeholder='Titulo'/>
+
+            <textarea name="descripcion" placeholder='Descripcion' id=""></textarea>
+
+            <input type='submit' value="Guardar" />
+
+        </form>
+
+    </div>
+  )
+}
