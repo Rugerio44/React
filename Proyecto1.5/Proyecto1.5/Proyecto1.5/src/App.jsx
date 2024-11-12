9ae821e8f8892b55e8719b59ce40c2b271acac8b
Lezama44 <giannieljefe@hotmail.com>
2024-11-12 14:11:11 -0600

    agregar un nuevo proyecto

---
+++ Proyecto1.5/Proyecto1.5/Proyecto1.5/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  
+
+  return (
+    <div className="App">  
+      <h1>Aprendiendo el usecontext con Gianni Francesco</h1>
+    </div>
+  );
+}
+
+export default App;
