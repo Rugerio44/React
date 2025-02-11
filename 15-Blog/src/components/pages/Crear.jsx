60a2bb11371a6498af08b141fcfa8f34fe25b1bb
Lezama44 <giannieljefe@hotmail.com>
2025-02-11 00:14:36 -0600

    agregar el crear pero tiene un bug

--- 15-Blog/src/components/pages/Crear.jsx
+++ 15-Blog/src/components/pages/Crear.jsx
@@ -1,9 +1,63 @@
 import React from 'react'
+import { useState } from 'react';
+import {useform} from '../../hooks/useform';
+import {Peticion} from '../../helpers/Peticion';
+import {Global} from '../../helpers/Global';
 
 export const Crear = () => {
+  
+    const {formulario, enviado,cambiado} = useform({});
+    const {resultado, setResultado} = useState(false);
+
+
+    const guardarArticulo = async (e) => {
+      e.preventDefault();
+
+      //Recoger datos del formulario
+      let nuevoArticulo = formulario;
+      console.log("Datos del formulario:", nuevoArticulo);
+
+      //Guardar evento en el backend
+      const {datos, cargando} = await Peticion(Global.url +"crear","POST", nuevoArticulo);
+
+      
+      if(datos.status === "success"){
+        setResultado(true);
+      }
+      
+      console.log(datos); 
+  }
+
+
   return (
-    <div>
-      <h2>Crear</h2>
+    <div className='jumbo'>
+      <h1>Crear Articulo</h1>
+      <p>Formulario para crear un articulo</p>
+      <pre> {JSON.stringify(formulario,null,2)} </pre>
+      <strong> {resultado ? "Articulo guardado correctamente": ""}</strong>
+      
+      {/*Crear el articuloo*/}
+      <form className='formulario' onSubmit={guardarArticulo} >
+
+        <div className='formulario__formulario-group'>
+           <label htmlFor="titulo" className='formulario__group-titulos'>Titulo</label>
+           <input onChange={cambiado} type="text" name='titulo'  className='formulario__group-box'/>
+        </div>
+
+        <div className='formulario__formulario-group'>
+           <label htmlFor="contenido" className='formulario__group-titulos'>Contenido</label>
+           <textarea onChange={cambiado} type="text" name='contenido' className='formulario__group-contenido'/>
+        </div>
+ 
+        <div className='formulario__formulario-group'>
+           <label htmlFor="file0" className='formulario__group-titulos'>Imagen</label>
+           <input onChange={cambiado} type="file" name='file0' id='file' className='formulario__group-boximage' />
+        </div>
+
+        <input type="submit" value='Guardar' className='formulario-guardar'/>
+        
+      </form>
+     
     </div>
   )
 }
