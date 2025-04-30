45c6d717632011347f999124eb390988093fbc3a
Lezama44 <giannieljefe@hotmail.com>
2025-04-29 22:54:26 -0600

    agregar el proyecto a netifly y decorar un poco el proyecto para que se vea presentable

--- 19-newMiduProyect/src/App.jsx
+++ 19-newMiduProyect/src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
 const users = [
-  { username: "midudev", nickname: "Gianni44", isFollowing: true },
-  { username: "pheralb", nickname: "PabloH", isFollowing: false },
-  { username: "dan_abramov", nickname: "Dan", isFollowing: true },
-  { username: "sarah_edo", nickname: "Sarah", isFollowing: false },
-  { username: "kentcdodds", nickname: "Kent", isFollowing: true }
+  { username: "Cristiano", nickname: "Gianni44", isFollowing: true },
+  { username: "elonmusk", nickname: "gianni", isFollowing: false },
+  { username: "elxokas", nickname: "XOKAS", isFollowing: true },
+  { username: "DebRyanShow", nickname: "Sarah", isFollowing: false },
+  { username: "realkarime", nickname: "Kent", isFollowing: true }
 ];
 
 function App() {
 
   return (
     <>
-    <section className='box'>
-      {
-        users.map(user => {
-          const {username,name,isFollowing} = user
-          return(
-            <TwitterFollowCard
-              key={username}
-              username={username}
-              initialIsfollowing={isFollowing}>
-              {name}
-            </TwitterFollowCard>
-          )
-        })
-      }
-    </section>
+    <div className='App'>
+      <h1>Twitter Follow Card</h1>
+      <section className='box'>
+        {
+          users.map(user => {
+            const {username,name,isFollowing} = user
+            return(
+              <TwitterFollowCard
+                key={username}
+                username={username}
+                initialIsfollowing={isFollowing}>
+                {name}
+              </TwitterFollowCard>
+            )
+          })
+        }
+      </section>
+    </div>
     </>
   )
 }
